63a4e03fdb534b5c1af682d5a232ea981049549e
Oliver Warden <oliverwarden@gmail.com>
2013-01-18 18:27:59 +0000

    NewGo launched by goList

--- src/ocept/gogo/GoList.java
+++ src/ocept/gogo/GoList.java
@@ -2,6 +2,7 @@ package ocept.gogo;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -18,8 +19,8 @@ public class GoList extends Activity {
 			
 			@Override
 			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				
+				Intent intent = new Intent(getBaseContext(), NewGo.class); //TODO chekc 1st parameter
+				startActivity(intent);
 			}
 		});
     }
---
+++ src/ocept/gogo/NewGo.java
@@ -0,0 +1,18 @@
+package ocept.gogo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+
+public class NewGo extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_go);
+		
+	}
+}
