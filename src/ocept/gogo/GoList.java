c608d2c5999d47c8c1fa6869df62e9bcbf813327
Oliver Warden <oliverwarden@gmail.com>
2013-01-18 17:01:53 +0000

    Inital commit, list and new go activities

---
+++ src/ocept/gogo/GoList.java
@@ -0,0 +1,34 @@
+package ocept.gogo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class GoList extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_go_list);
+        
+        Button newGoButton = (Button) findViewById(R.id.newGoButton);
+        newGoButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_go_list, menu);
+        return true;
+    }
+    
+}
