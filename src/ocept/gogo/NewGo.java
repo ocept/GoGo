50a7d8400320fc1b63911950fb399c937b85d908
Oliver Warden <oliverwarden@gmail.com>
2013-01-19 18:36:26 +0000

    Save new gos to db

--- src/ocept/gogo/NewGo.java
+++ src/ocept/gogo/NewGo.java
@@ -1,18 +1,64 @@
 package ocept.gogo;
 
+import ocept.gogo.db.*;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 public class NewGo extends Activity {
-
+	goDB dba;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_go);
 		
+        //init db
+        dba = new goDB(this);
+
+        //connect up view
+        Button okButton = (Button) findViewById(R.id.newGoOkButton);
+        okButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				try{
+					saveGo();
+				}
+				catch(Exception e){
+					e.printStackTrace();
+				}
+			}
+		});
+        
+        ((Button) findViewById(R.id.newGoCancelButton)).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				closeActivity();
+			}
+		});
+	}
+	private void saveGo(){
+		//get go data
+		Go newGo = new Go();
+		EditText name = (EditText) findViewById(R.id.newGoName);
+		newGo.Name = name.getText().toString();
+		newGo.Desc = ((EditText) findViewById(R.id.newGoDesc)).getText().toString();
+		
+		//open and enter into db
+        dba.open();
+		dba.insertGo(newGo);
+		dba.close();
+		
+		//close activity
+		this.finish();
+	}
+	private void closeActivity()
+	{
+		this.finish();
 	}
 }
+
