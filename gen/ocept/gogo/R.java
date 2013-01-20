43c331b3aefd60e7972e2f253e03e1df3f38da75
Oliver Warden <oliverwarden@gmail.com>
2013-01-20 18:19:27 +0000

    Add listview

--- gen/ocept/gogo/R.java
+++ gen/ocept/gogo/R.java
@@ -14,13 +14,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int linearLayout1=0x7f070000;
-        public static final int menu_settings=0x7f070006;
-        public static final int newGoButton=0x7f070001;
-        public static final int newGoCancelButton=0x7f070004;
-        public static final int newGoDesc=0x7f070003;
-        public static final int newGoName=0x7f070002;
-        public static final int newGoOkButton=0x7f070005;
+        public static final int listView1=0x7f070001;
+        public static final int menu_settings=0x7f070007;
+        public static final int newGoButton=0x7f070002;
+        public static final int newGoCancelButton=0x7f070005;
+        public static final int newGoDesc=0x7f070004;
+        public static final int newGoName=0x7f070003;
+        public static final int newGoOkButton=0x7f070006;
+        public static final int scrollView1=0x7f070000;
     }
     public static final class layout {
         public static final int activity_go_list=0x7f030000;
