b7e7edbafcfa382e5eded6ac856f69220648e715
Oliver Warden <oliverwarden@gmail.com>
2013-01-30 20:21:38 +0000

    setup context menu framework

--- gen/ocept/gogo/R.java
+++ gen/ocept/gogo/R.java
@@ -12,11 +12,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_menu_close_clear_cancel=0x7f020001;
+        public static final int ic_menu_delete=0x7f020002;
+        public static final int ic_menu_edit=0x7f020003;
     }
     public static final class id {
         public static final int checkDone=0x7f070007;
         public static final int descText=0x7f070006;
-        public static final int menu_settings=0x7f070008;
+        public static final int item1=0x7f070008;
+        public static final int menu_delete=0x7f070009;
         public static final int name=0x7f070005;
         public static final int newGoButton=0x7f070000;
         public static final int newGoCancelButton=0x7f070003;
@@ -31,10 +35,12 @@ public final class R {
     }
     public static final class menu {
         public static final int activity_go_list=0x7f060000;
+        public static final int go_list_context=0x7f060001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int menu_delete=0x7f040006;
         public static final int menu_settings=0x7f040002;
         public static final int newGoButtonText=0x7f040003;
         public static final int newGoDescHint=0x7f040005;
--- src/ocept/gogo/GoList.java
+++ src/ocept/gogo/GoList.java
@@ -13,15 +13,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.util.Log;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView.MultiChoiceModeListener;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -51,6 +57,59 @@ public class GoList extends ListActivity {
 				startActivity(intent);
 			}
 		});
+        
+        //set up context menu
+        initCAM();
+    }
+    
+    private void initCAM()
+    {
+    	final ListView listView = getListView();
+    	
+    	
+    	listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+    	listView.setMultiChoiceModeListener(new MultiChoiceModeListener() {
+
+    		@Override
+    	    public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+    	        // Here you can do something when items are selected/de-selected,
+    	        // such as update the title in the CAB
+    	    }
+
+    	    @Override
+    	    public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+    	        // Respond to clicks on the actions in the CAB
+    	        switch (item.getItemId()) {
+    	            case R.id.menu_delete:
+    	                //TODO deleteSelectedItems();
+    	                mode.finish(); // Action picked, so close the CAB
+    	                return true;
+    	            default:
+    	                return false;
+    	        }
+    	    }
+
+    	    @Override
+    	    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+    	        // Inflate the menu for the CAB
+    	        MenuInflater inflater = mode.getMenuInflater();
+    	        inflater.inflate(R.menu.go_list_context, menu);
+    	        return true;
+    	    }
+    	    
+    	    @Override
+    	    public void onDestroyActionMode(ActionMode mode) {
+    	        // Here you can make any necessary updates to the activity when
+    	        // the CAB is removed. By default, selected items are deselected/unchecked.
+    	    }
+
+    	    @Override
+    	    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+    	        // Here you can perform updates to the CAB due to
+    	        // an invalidate() request
+    	        return false;
+    	    }
+    	});
     }
     
     @Override
