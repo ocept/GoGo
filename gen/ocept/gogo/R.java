1ab9fdca741a2609cd039f4bd9dfa6261474b5c6
Oliver Warden <oliverwarden@gmail.com>
2013-02-08 18:35:41 +0000

    number picker and layout tweaks

--- gen/ocept/gogo/R.java
+++ gen/ocept/gogo/R.java
@@ -25,16 +25,17 @@ public final class R {
         public static final int row_selected=0x7f020004;
     }
     public static final class id {
-        public static final int checkDone=0x7f080007;
-        public static final int descText=0x7f080006;
-        public static final int item1=0x7f080008;
-        public static final int menu_delete=0x7f080009;
-        public static final int name=0x7f080005;
+        public static final int bountyPicker=0x7f080005;
+        public static final int checkDone=0x7f080008;
+        public static final int descText=0x7f080007;
+        public static final int item1=0x7f080009;
+        public static final int menu_delete=0x7f08000a;
+        public static final int name=0x7f080006;
         public static final int newGoButton=0x7f080000;
-        public static final int newGoCancelButton=0x7f080003;
+        public static final int newGoCancelButton=0x7f080004;
         public static final int newGoDesc=0x7f080002;
         public static final int newGoName=0x7f080001;
-        public static final int newGoOkButton=0x7f080004;
+        public static final int newGoOkButton=0x7f080003;
     }
     public static final class layout {
         public static final int activity_go_list=0x7f030000;
@@ -47,12 +48,14 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int cancel=0x7f050007;
         public static final int hello_world=0x7f050001;
         public static final int menu_delete=0x7f050006;
         public static final int menu_settings=0x7f050002;
         public static final int newGoButtonText=0x7f050003;
         public static final int newGoDescHint=0x7f050005;
         public static final int newGoNameHint=0x7f050004;
+        public static final int ok=0x7f050008;
     }
     public static final class style {
         /** 
